05ec0e0008bcc28812251675a1cfabc0fd4b770f
ali <ali@alis-iMac.local>
2019-06-25 00:53:35 +0900

    Adds unit tests

--- src/positive-integer-interface.php
+++ src/positive-integer-interface.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Zen\Types;
+namespace zen\types;
 
 
 interface PositiveIntegerInterface
--- src/positive-integer.php
+++ src/positive-integer.php
@@ -3,7 +3,28 @@ declare(strict_types=1);
 
 namespace zen\types;
 
+use \InvalidArgumentException;
+
 class PositiveInteger implements PositiveIntegerInterface
 {
+    private $integer;
 
+    public function __construct(int $input)
+    {
+        if(!$input > 0){
+            if($input === 0){
+                throw new InvalidArgumentException("Given value '$input' is zero and can not be accepted as a positive integer", 1001);
+            }
+            if($input < 0){
+                throw new InvalidArgumentException("Given value '$input' is negative and can not be accepted as a positive integer", 1002);
+            }
+        }
+
+        $this->integer = $input;
+    }
+
+    public function value(): int
+    {
+        return $this->integer;
+    }
 }
